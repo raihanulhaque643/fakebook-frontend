352ecd4b20b73d2ac8b9620e8ad31c8c80735928
Raihanul Haque <raihan.hd.500@gmail.com>
2021-04-10 17:15:28 +0600

    agree to an opinion api implemented

--- src/components/Opinion.js
+++ src/components/Opinion.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { DeleteModal } from './DeleteModal'
+import axios from 'axios';
 
 const Opinion = ({ opinionId, author, firstName, lastName, image, description, agreements, disagreements, date, owner, setReFetchOpinions }) => {
 
@@ -12,6 +13,37 @@ const Opinion = ({ opinionId, author, firstName, lastName, image, description, a
     const hours = dateHolder.getHours();
     const minutes = dateHolder.getMinutes();
     const seconds = dateHolder.getSeconds();
+
+    const myToken = localStorage.getItem('token')
+
+    const handleAgree = () => {
+        console.log(JSON.parse(localStorage.getItem('user'))._id + ' has agreed to' + opinionId )
+
+        let formData = {};
+
+        formData['userId'] = JSON.parse(localStorage.getItem('user'))._id
+
+        console.log(formData)
+
+        axios({
+            url: `https://fakebook-backend-643.herokuapp.com/opinion/agree/${opinionId}`,
+            method: 'PATCH',
+            headers: {
+                authorization: myToken
+            },
+            body: formData
+        }).then((response) => {
+            console.log(response)
+        }).catch((e) => {
+            console.log('could not write to database')
+            console.log(e.response.data)
+        })
+
+    }
+
+    const handleDisagree = () => {
+
+    }
 
     return (
         <div className="max-w-lg min-w-lg mx-auto my-4 bg-blue-100">
@@ -38,8 +70,12 @@ const Opinion = ({ opinionId, author, firstName, lastName, image, description, a
                 </div>
                 <div className="flex flex-row my-2 px-2 justify-between">
                     <div className="">
-                        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 mr-4 border rounded shadow focus:outline-none text-green-600">Agree</button>
-                        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 mr-0 border rounded shadow focus:outline-none text-red-600">Disagree</button>
+                        <button
+                        onClick={() => handleAgree()}
+                        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 mr-4 border rounded shadow focus:outline-none text-green-600">Agree</button>
+                        <button
+                        onClick={() => handleDisagree()}
+                        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 mr-0 border rounded shadow focus:outline-none text-red-600">Disagree</button>
                     </div>
                     <div className="flex flex-row">
                         <div className="py-2 px-4 font-semibold rounded bg-green-600 mr-4 text-white">{agreements}</div>
